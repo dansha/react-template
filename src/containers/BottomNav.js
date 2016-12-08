d5ac07b03f7239f6e1dfcfe744d3426352ac168b
dan shamir <dan shamir>
2016-12-08 11:59:24 +0200

    removed extran file

--- src/containers/BottomNav.js
+++
@@ -1,9 +0,0 @@
-import React ,{Component} from 'react';
-import { withRouter } from 'react-router';
-import BottomNavView from '../components/BottomNav';
-
-class BottomNav extends Component{
-
-}
-
-export default withRouter(BottomNavView);
